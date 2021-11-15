f714acbee610caf371db4243f481e36641a26de1
wangziyi <1582446372.qq.com>
2021-11-15 15:42:55 +0800

    wzy实现上传

---
+++ src/main/java/com/kuang/SpringMybatis04Application.java
@@ -0,0 +1,13 @@
+package com.kuang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMybatis04Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMybatis04Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/kuang/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.kuang.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Mapper//添加了这个注解，表示了这是mybatis的mapper类
+@Repository//说明被Spring整合了
+public class UserMapper {
+//    查询所有用户
+//    查询一个用户
+//    增加一个用户
+//    修改一个用户
+//    删除一个用户
+
+
+}
