80f874f2bd86b000dbd5b50759db80ea277ba18b
wangziyi <1582446372.qq.com>
2021-11-15 15:57:19 +0800

    王紫怡第四次提交

--- src/main/java/com/kuang/pojo/Teacher.java
+++ src/main/java/com/kuang/pojo/Teacher.java
@@ -1,12 +1,12 @@
-package com.kuang.service;
+package com.kuang.pojo;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
-public class UserService {
+@NoArgsConstructor
+public class Teacher {
     private Integer id;
 }
